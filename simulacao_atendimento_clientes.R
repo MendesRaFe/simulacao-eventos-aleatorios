ded6eb201701c936ebba264716b322b55f759f9d
MendesRaFe <135549408+MendesRaFe@users.noreply.github.com>
2024-05-08 12:59:13 -0300

    Update simulacao_atendimento_clientes.R
    criada função auxiliar para o erro padrão e adicionado código para plotar gráfico de linha

--- simulacao_atendimento_clientes.R
+++ simulacao_atendimento_clientes.R
@@ -1,4 +1,11 @@
-## Balcão de Atendimento com fila ##
+## Balcão de Atendimento com Fila ##
+
+# Função auxiliar para cálculo do erro padrão
+erro_padrao <- function(dados) {
+  dp <- sd(dados)
+  st_err <- dp / sqrt(length(dados))
+  return(st_err)
+}
 
 # Faz a simulação de uma sequêcia de atendimento
 atendimento <- function(tempo_total, rate, n, mi) {
@@ -11,7 +18,7 @@ atendimento <- function(tempo_total, rate, n, mi) {
   tdisp <- numeric(n)
   tcheg <- numeric(0)
 
-  # Gera os intervalos de tempo entre um cliente e o próximo até o encerramento do tempo total de atendimento
+  # Gera intervalos de tempo entre atendimentos até seu encerramento
   while (TRUE) {
     z <- rexp(1, rate)
     tempo_acum <- tempo_acum + z
@@ -44,42 +51,85 @@ atendimento <- function(tempo_total, rate, n, mi) {
               w = prop_naoatendidos, tmax = tempo_max))
 }
 
-simula_atendimentos <- function(nb, c) {
-  vetor_medias_x <- numeric(0)
-  vetor_medias_y <- numeric(0)
-  vetor_medias_w <- numeric(0)
-  vetor_medias_tmax <- numeric(0)
+# Simula N atendentimentos por blocos de 500 e aplica o teor. do limite central
+simula_atendimentos <- function(t, rate, n, mi, nb, c) {
+  # Inicialização de vetores de interesse
+  tam <- c(nb)
   x_elem <- numeric(0)
   y_elem <- numeric(0)
   w_elem <- numeric(0)
   tmax_elem <- numeric(0)
-  tam <- c(nb)
+  vetor_w_err <- numeric(0)
+  vetor_tmax_err <- numeric(0)
+  vetor_medias_x <- numeric(0)
+  vetor_medias_y <- numeric(0)
+  vetor_medias_w <- numeric(0)
+  vetor_medias_tmax <- numeric(0)
 
   while (TRUE) {
+    # obtem os dados cumulados a cada 500 simulações de atendimento
     for (i in 1:500) {
-      resultado <- atendimento(tempo_total = 50, rate = 3, n = 5, mi = 0.5)
+      resultado <- atendimento(t, rate, n, mi)
       x_elem <- c(x_elem, resultado$x)
       y_elem <- c(y_elem, resultado$y)
       w_elem <- c(w_elem, resultado$w)
       tmax_elem <- c(tmax_elem, resultado$tmax)
     }
+    # vetores resultantes com as médias efetuadas a cada 500 iterações
     vetor_medias_x <- c(vetor_medias_x, mean(x_elem))
     vetor_medias_y <- c(vetor_medias_y, mean(y_elem))
     vetor_medias_w <- c(vetor_medias_w, mean(w_elem))
     vetor_medias_tmax <- c(vetor_medias_tmax, mean(tmax_elem))
 
-    dp <- sd(w_elem)
-    st_err <- dp / sqrt(length(w_elem))
-    threshold <- 2 * 1.96 * st_err
+    # erros padrão de tmax e de w a cada 500 iterações
+    tmax_err <- erro_padrao(tmax_elem)
+    vetor_tmax_err <- c(vetor_tmax_err, tmax_err)
+    w_err <- erro_padrao(w_elem)
+    vetor_w_err <- c(vetor_w_err, w_err)
+
+    # faz a convergência em relação ao erro padrão de w
+    threshold <- 2 * 1.96 * w_err
     if (threshold < c)
       break
     tam <- c(tam, max(tam) + 500)
   }
-  return(list(x_k = vetor_medias_x, y_k = vetor_medias_y,
-              w_k = vetor_medias_w, tmax_k = vetor_medias_tmax, k = tam))
+  return(list(x_k = vetor_medias_x, y_k = vetor_medias_y, w_k = vetor_medias_w,
+              tmax_k = vetor_medias_tmax, k = tam,
+              tmax_ep = vetor_tmax_err, w_ep = vetor_w_err))
 }
 
-simulacao <- simula_atendimentos(nb = 500, c = 0.002)
-print(simulacao$w_k)
-print(simulacao$tmax_k)
-print(simulacao$k)
+# Grava o resultado do experimento na variável simulacao
+simulacao <- simula_atendimentos(t = 50, rate = 3, n = 5, mi = 0.5,
+                                 nb = 500, c = 0.002)
+
+# Gera dataframe com a variável de interesse referentes a W
+df1 <- cbind.data.frame(simulacao$k, simulacao$w_k, simulacao$w_ep)
+names(df1)[names(df1) == "simulacao$w_k"] <- "W"
+names(df1)[names(df1) == "simulacao$k"] <- "N"
+names(df1)[names(df1) == "simulacao$w_ep"] <- "erro_padrao"
+
+# Adiciona ao df1 as colunas que definem o intervalo de confiança de 95%
+df1$lim_inf <- df1$W - 1.96 * df1$erro_padrao
+df1$lim_sup <- df1$W + 1.96 * df1$erro_padrao
+
+# Gera o gráfico de linha com as médias parciais de W e o intervalo de confiança
+plot(df1$N, df1$W, type = "l", col = "red", xlab = "N", ylab = "W",
+     ylim = range(c(df1$W, df1$lim_inf, df1$lim_sup)))
+lines(df1$N, df1$lim_inf, lty = "dotted")
+lines(df1$N, df1$lim_sup, lty = "dotted")
+
+# Gera dataframe com a variável de interesse referente a média de tempo máximo
+df2 <- cbind.data.frame(simulacao$k, simulacao$tmax_k, simulacao$tmax_ep)
+names(df2)[names(df2) == "simulacao$tmax_k"] <- "Tmax"
+names(df2)[names(df2) == "simulacao$k"] <- "N"
+names(df2)[names(df2) == "simulacao$tmax_ep"] <- "erro_padrao"
+
+# Adiciona ao df2 as colunas que definem o intervalo de confiança de 95%
+df2$lim_inf <- df2$Tmax - 1.96 * df2$erro_padrao
+df2$lim_sup <- df2$Tmax + 1.96 * df2$erro_padrao
+
+# Gera o gráfico de linha com médias parciais de Tmax e o intervalo de confiança
+plot(df2$N, df2$Tmax, type = "l", col = "red", xlab = "N", ylab = "Tmax",
+     ylim = range(c(df2$Tmax, df2$lim_inf, df2$lim_sup)))
+lines(df2$N, df2$lim_inf, lty = "dotted")
+lines(df2$N, df2$lim_sup, lty = "dotted")
